23c7298c48223226b8b0685600d79842f2e75f24
Jajodia <tjajodia@internal.tavisca.com>
2019-08-22 15:17:22 +0530

    CSS and JS code Restructured

--- js/main-script.js
+++ js/main-script.js
@@ -1,28 +1,5 @@
 var tasks = [];
 
-function addButtonClicked(){
-    addTaskInStorage();
-    renderTaskInAsHTML(tasks);
-    AddButtonHide();
-    searchButtonShow();
-}
-
-function addTaskInStorage(){
-    let task = getValueFromTextBox();
-    clearSearchFiedl();
-    tasks.push(task);
-
-}
-
-function getValueFromTextBox(){
-    let text = document.getElementsByClassName("input-field")[0].value;
-    return text;
-}
-
-function clearSearchFiedl(){
-    document.getElementsByClassName("input-field")[0].value = "";
-}
-
 function renderTaskInAsHTML(tasks){
     removeOldRows();
     for(let i = 0; i < tasks.length; i++){
@@ -59,36 +36,9 @@ function renderTaskInAsHTML(tasks){
     }
 }
 
-function deleteItem(element){
-    let indexInTasks = element.parentNode.parentNode.childNodes[0].innerText;
-    tasks.splice(indexInTasks,1);
-    renderTaskInAsHTML(tasks);
-}
-
-function modifyItem(element){
-    let editField = element.parentNode.parentNode.children[1];
-    let previousText = editField.innerHTML;
-    let immediateParent = element.parentNode;
-    immediateParent.innerHTML = `<i class="fa fa-check fa-2x" aria-hidden="true" onclick="editedButtonClicked(this)"></i>`;
-    
-    editField.innerHTML = `<input class="edit-field" type="textarea" value="${previousText}">`;
-}
-
-
-function editedButtonClicked(element){
-    let updatedValue = element.parentNode.parentNode.children[1].children[0].value;
-    let indexInTasks = element.parentNode.parentNode.children[0].innerText;
-    replaceValue(indexInTasks,updatedValue);
-}
-
-function replaceValue(index, value){
-    tasks[index] = value;
-    renderTaskInAsHTML(tasks); 
-}
-
+// Button Events
 
 function searchButtonClicked(event){
-
     if(event.keyCode == '13' && document.getElementsByClassName("fa-plus")[0].style.display == 'inline-block'){
         addButtonClicked();
     }
@@ -118,6 +68,42 @@ function searchButtonClicked(event){
     
 }
 
+function addButtonClicked(){
+    addTaskInStorage();
+    renderTaskInAsHTML(tasks);
+    AddButtonHide();
+    searchButtonShow();
+}
+
+function editedButtonClicked(element){
+    let updatedValue = element.parentNode.parentNode.children[1].children[0].value;
+    let indexInTasks = element.parentNode.parentNode.children[0].innerText;
+    replaceValue(indexInTasks,updatedValue);
+
+    function replaceValue(index, value){
+        tasks[index] = value;
+        renderTaskInAsHTML(tasks); 
+    }
+    
+}
+
+function modifyItem(element){
+    let editField = element.parentNode.parentNode.children[1];
+    let previousText = editField.innerHTML;
+    let immediateParent = element.parentNode;
+    immediateParent.innerHTML = `<i class="fa fa-check fa-2x" aria-hidden="true" onclick="editedButtonClicked(this)"></i>`;
+    
+    editField.innerHTML = `<input class="edit-field" type="textarea" value="${previousText}">`;
+}
+
+function deleteItem(element){
+    let indexInTasks = element.parentNode.parentNode.childNodes[0].innerText;
+    tasks.splice(indexInTasks,1);
+    renderTaskInAsHTML(tasks);
+}
+
+
+// Helper Functions
 
 function AddButtonHide(){
     let addButton = document.getElementsByClassName("fa-plus")[0];
@@ -139,3 +125,17 @@ function searchButtonShow(){
     addButton.style.display = 'inline-block';
 }
 
+function getValueFromTextBox(){
+    let text = document.getElementsByClassName("input-field")[0].value;
+    return text;
+}
+
+function clearSearchFiedl(){
+    document.getElementsByClassName("input-field")[0].value = "";
+}
+
+function addTaskInStorage(){
+    let task = getValueFromTextBox();
+    clearSearchFiedl();
+    tasks.push(task);
+}
