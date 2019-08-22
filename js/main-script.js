801a0d56994f5c1b34a77d79d9e18927f47b6556
Jajodia <tjajodia@internal.tavisca.com>
2019-08-22 11:48:09 +0530

    Added Addition and Deletion functionality

--- js/main-script.js
+++ js/main-script.js
@@ -1,28 +1,64 @@
-
 var tasks = [];
 
-function addTask(task){
-    notes.push(task);
+function addButtonClicked(){
+    addTaskInStorage();
+    renderTaskInAsHTML();
 }
 
-function removeTask(task){
-    notes.splice(task)
+function addTaskInStorage(){
+    let task = getValueFromTextBox();
+    tasks.push(task);
+
+    function getValueFromTextBox(){
+        let text = document.getElementsByClassName("input-field")[0].value;
+        return text;
+    }
 }
 
-function getValueFromTextBox(){
-    let text = document.getElementsByClassName("input-field")[0].value;
-    return text;
-}
-
-function hasElement(taskKey){
-
-}
-
-function renderTask(){
-
+function renderTaskInAsHTML(){
+    removeOldRows();
     for(let i = 0; i < tasks.length; i++){
-        
+        let row = makeRowHTML(i,tasks[i]);
+        insertRowInTable(row);
     }
 
+    function insertRowInTable(row){
+        let table = document.getElementsByClassName("task-row")[0].parentNode;
+        let newRow = table.insertRow(table.rows.length);
+        newRow.innerHTML = row;
+    }
+
+    function makeRowHTML(taskNumber, task){
+        let row = `<tr>
+                        <td style="display: none;">${taskNumber}</td>
+                        <td>${task}</td>
+                        <td>
+                        <i class="fa fa-trash" onclick="deleteItem(this);"></i>
+                        <i class="fa fa-wrench" onclick="modifyItem(this);"></i>
+                        </td>
+                        </tr>`;
+                        return row;
+    }
+
+    function removeOldRows(){
+        let table = document.getElementsByClassName("task-row")[0].parentNode;
+        let rowNumber = table.rows.length;
+        
+        while(rowNumber > 2){
+            table.deleteRow(--rowNumber);
+        }
+
+    }
 }
+
+function deleteItem(element){
+    let indexInTasks = element.parentNode.parentNode.firstElementChild.innerText;
+    tasks.splice(indexInTasks,1);
+    renderTaskInAsHTML();
+}
+
+
+
+
+
 
