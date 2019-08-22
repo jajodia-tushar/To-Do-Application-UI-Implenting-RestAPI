81d7eda92211a60a118e0053299bdcf0b63d32cd
Jajodia <tjajodia@internal.tavisca.com>
2019-08-22 10:40:13 +0530

    search bar and UI completed

--- js/main-script.js
+++ js/main-script.js
@@ -1,1 +1,28 @@
-s
+
+var tasks = [];
+
+function addTask(task){
+    notes.push(task);
+}
+
+function removeTask(task){
+    notes.splice(task)
+}
+
+function getValueFromTextBox(){
+    let text = document.getElementsByClassName("input-field")[0].value;
+    return text;
+}
+
+function hasElement(taskKey){
+
+}
+
+function renderTask(){
+
+    for(let i = 0; i < tasks.length; i++){
+        
+    }
+
+}
+
